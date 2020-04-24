9500670be582c8104a3e6808132d95958a53d2aa
Rizal Hilman <rizal@udacoding.com>
2020-04-24 11:19:53 +0700

    initial commit

---
+++ Sample Git Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample Git Project
+//
+//  Created by Rizal Hilman on 24/04/20.
+//  Copyright © 2020 Rizal Hilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
